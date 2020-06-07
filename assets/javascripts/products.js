a8b5b357ef4951456cf7d7e2549cc99d81bc076e
Dimitri <andre.d3003@gmail.com>
2020-06-07 19:15:02 +0200

    Filter by color forEach and const

--- assets/javascripts/products.js
+++ assets/javascripts/products.js
@@ -12,65 +12,65 @@ let hat = {
 }
 
 
-function Hat(name, price, color, image) {
+function Accessory(name, price, color, image) {
   this.name = name;
   this.price = price;
   this.color = color;
   this.image = image;
 }
 
-Hat.prototype.toString = function() {
+Accessory.prototype.toString = function() {
   return (this.name + ", color: " + this.color + ", price: " + this.price + ", image: " + this.image);
 }
 
 
 let hats = [
-  new Hat('Baseball cap', '11.99', 'Red', 'assets/images/red/hats/1.png'),
-  new Hat('Baseball cap', '11.99', 'Blue', 'assets/images/blue/hats/1.png'),
-  new Hat('Baseball cap', '11.99', 'Yellow', 'assets/images/yellow/hats/1.png'),
-  new Hat('Baseball cap', '11.99', 'Green', 'assets/images/green/hats/1.png'),
-  new Hat('Beanie', '17.99', 'Red', 'assets/images/red/hats/2.png'),
-  new Hat('Beanie', '17.99', 'Blue', 'assets/images/blue/hats/2.png'),
-  new Hat('Beanie', '17.99', 'Green', 'assets/images/green/hats/2.png'),
-  new Hat('Straw hat', '10.99', 'Yellow', 'assets/images/yellow/hats/3.png'),
-  new Hat('Straw hat', '10.99', 'Blue', 'assets/images/blue/hats/3.png'),
-  new Hat('Trilby', '10.99', 'Red', 'assets/images/red/hats/4.png'),
-  new Hat('Trilby', '10.99', 'Blue', 'assets/images/blue/hats/4.png'),
-  new Hat('Trilby', '10.99', 'Yellow', 'assets/images/yellow/hats/4.png'),
+  new Accessory('Baseball cap', '11.99', 'red', 'assets/images/red/hats/1.png'),
+  new Accessory('Baseball cap', '11.99', 'blue', 'assets/images/blue/hats/1.png'),
+  new Accessory('Baseball cap', '11.99', 'yellow', 'assets/images/yellow/hats/1.png'),
+  new Accessory('Baseball cap', '11.99', 'green', 'assets/images/green/hats/1.png'),
+  new Accessory('Beanie', '17.99', 'red', 'assets/images/red/hats/2.png'),
+  new Accessory('Beanie', '17.99', 'blue', 'assets/images/blue/hats/2.png'),
+  new Accessory('Beanie', '17.99', 'green', 'assets/images/green/hats/2.png'),
+  new Accessory('Straw hat', '10.99', 'yellow', 'assets/images/yellow/hats/3.png'),
+  new Accessory('Straw hat', '10.99', 'blue', 'assets/images/blue/hats/3.png'),
+  new Accessory('Trilby', '10.99', 'red', 'assets/images/red/hats/4.png'),
+  new Accessory('Trilby', '10.99', 'blue', 'assets/images/blue/hats/4.png'),
+  new Accessory('Trilby', '10.99', 'yellow', 'assets/images/yellow/hats/4.png'),
 ];
 
-function displayHat(hat) {
+function displayAccessory(accessory) {
 
   //Creating the HTML element
 
-  let newHat = document.createElement('div');
-  newHat.className += 'accessory col-sm-4 ' + hat.color;
+  let newAccessory = document.createElement('div');
+  newAccessory.className += 'accessory col-sm-4 ' + accessory.color;
 
   let my3 = document.createElement('div');
   my3.className += 'card my-3';
 
   let currency = document.createElement('div');
   currency.className += 'currency btn btn-light disabled';
-  currency.textContent = hat.price;
+  currency.textContent = accessory.price;
 
   let image = document.createElement('img');
   image.className += 'card-img-top';
-  image.src += hat.image;
-  image.alt = 'Image of ' + hat.name;
+  image.src += accessory.image;
+  image.alt = 'Image of ' + accessory.name;
 
   let cardbody = document.createElement('div');
   cardbody.className += 'card-body text-center';
 
   let cardtitle = document.createElement('h5');
   cardtitle.className += 'card-title';
-  cardtitle.textContent = hat.name;
+  cardtitle.textContent = accessory.name;
 
   let cardtext = document.createElement('p');
   cardtext.className += 'card-text';
   cardtext.textContent = 'Color: '
 
   let em = document.createElement('em');
-  em.textContent = hat.color;
+  em.textContent = accessory.color;
 
   cardtext.appendChild(em);
 
@@ -86,66 +86,76 @@ function displayHat(hat) {
   my3.appendChild(image);
   my3.appendChild(cardbody);
 
-  newHat.appendChild(my3);
+  newAccessory.appendChild(my3);
 
-  return newHat;
+  return newAccessory;
 }
 
 let products = document.getElementById('products')
 products.innerHTML = null;
 
-for (i = 0; i < hats.length; i++) {
-  products.appendChild(displayHat(hats[i]));
-}
+hats.forEach(elem => products.appendChild(displayAccessory(elem)));
 
+/*----------------
+//FILTER BUTTONS
+---------------*/
 
-//Create all button
-let filterButtons = document.querySelectorAll('.btn-group .btn')
+const filterButtonsContainer = document.querySelector('.btn-group')
+
+const filterButtons = filterButtonsContainer.childNodes;
 
 let allButton = document.createElement('button');
 allButton.setAttribute('type', 'button');
 allButton.className = 'btn btn-outline-secondary';
 allButton.textContent = 'All';
 
-console.log(filterButtons.length);
-console.log(filterButtons);
-filterButtons.item(filterButtons.length - 1).parentNode.appendChild(allButton);
+filterButtonsContainer.appendChild(allButton);
 
-let filterButtons2 = document.querySelectorAll('.btn-group .btn');
-console.log(filterButtons2.length);
-console.log(filterButtons2);
 //Filter by Color
 
-for (i = 0; i < filterButtons2.length; i++) {
-  filterButtons2[i].className = 'btn btn-outline-secondary';
+function highlightSelectedFilter(el) {
+  filterButtons.forEach(function(el) {
+    el.className = 'btn btn-outline-secondary'
+  });
+  el.className += ' active';
 }
 
-for (i = 0; i < filterButtons2.length; i++) {
-  filterButtons2[i].addEventListener('click',
-    function highlightSelectedFilter() {
-      for (i = 0; i < filterButtons2.length; i++) {
-        filterButtons2[i].className = 'btn btn-outline-secondary';
-      }
-      this.className += ' active'
-    }, false)
+filterButtons.forEach(elem => elem.className = 'btn btn-outline-secondary');
+
+filterButtons.forEach(elem =>
+  elem.addEventListener('click',
+    function() {
+      highlightSelectedFilter(elem)
+    },
+    false)
+);
+
+
+
+//addEventListener click filter filterButtons
+
+const allProducts = products.childNodes;
+
+function filterAccesoriesByColor(el) {
+  for (i = 0; i < allProducts.length; i++) {
+    allProducts.item(i).style.display = 'none';
+    const arrayClass = allProducts.item(i).className.split(' ')
+    if (arrayClass[arrayClass.length - 1] == el.textContent.toLowerCase()) {
+      allProducts.item(i).style.display = 'block';
+    } else if (el.textContent == 'All') {
+      allProducts.item(i).style.display = 'block';
+    } else {}
+  }
 }
 
-let allProducts = products.children;
+filterButtons.forEach(el =>
+  el.addEventListener('click',
+    function() {
+      filterAccesoriesByColor(el);
+    },
+    false)
+)
 
-//addEventListener click filter filterButtons2
-
-for (i = 0; i < filterButtons2.length; i++) {
-  filterButtons2[i].addEventListener('click',
-    function filterHatsByColor() {
-      for (i = 0; i < allProducts.length; i++) {
-        allProducts.item(i).style.display = 'none';
-        let arrayClass = allProducts.item(i).className.split(' ')
-        if (arrayClass[arrayClass.length - 1] == this.textContent) {
-          allProducts.item(i).style.display = 'block';
-        } else if (this.textContent == 'All') {
-          allProducts.item(i).style.display = 'block';
-        } else {}
-      }
-
-    }, false)
-}
+/* ----------------------------
+AJAX function to retrieve JSON
+------------------------------*/
